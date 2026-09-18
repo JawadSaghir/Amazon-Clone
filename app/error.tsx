2570def7d2a16b288034e31c958f721e1c5d759d
jawadsaghir <jawadsaghire12@gmail.com>
2026-09-19 02:26:02 +0500

    add app/error.tsx

---
+++ app/error.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function ErrorPage({ reset }: { reset: () => void }) {
+  return (
+    <div className="page-shell py-16 text-center">
+      <div className="panel mx-auto max-w-xl p-10">
+        <h1 className="text-3xl font-black">Something went wrong</h1>
+        <button type="button" onClick={reset} className="brass-button mt-6">
+          Try again
+        </button>
+      </div>
+    </div>
+  );
+}
